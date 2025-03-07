c41f71b8a1c5c5d56387e81a1775ae41fb465294
Xmarimarquesh <marimarquesh39@gmail.com>
2025-03-07 17:13:27 -0300

    ta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,67 @@
+"use client";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [pokemonId, setPokemonId] = useState<number>(1);
+  const [message, setMessage] = useState<string>('');
+  const [capturedPokemons, setCapturedPokemons] = useState<any[]>([]);
+
+  const capturePokemon = async () => {
+    try {
+      const response = await axios.post('/localhost:/8080/capture', { id: pokemonId }); 
+      setMessage(`Pokémon ${response.data.pokemon.name} capturado com sucesso!`);
+    } catch (error:any) {
+      setMessage(error.response?.data?.message || 'Erro na captura!');
+    }
+  };
+
+  const fetchCapturedPokemons = async () => {
+    try {
+      const response = await axios.get('/team');
+      setCapturedPokemons(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar Pokémons capturados');
+    }
+  };
+
+  return (
+    <div className="bg-root flex flex-col items-center">
+      <Image src={require('@/assets/capa.jpg')} alt="" className="h-100 object-cover"></Image>
+      <div className="h-30">
+        <h1 className="text-amber-500 font-bold text-[2.4em] mt-10 hover:text-[2.6em] cursor-pointer transform-stroke">Pokemon - API</h1>
+      </div>
+      <div className="h-30">
+        <button 
+          onClick={capturePokemon}
+          className="text-white bg-amber-500 m-10 p-4 rounded-md font-bold text-2xl hover:bg-amber-600">
+          Tentar capturar
+        </button>
+      </div>
+      <div className="w-[80%] bg-white h-[1px]"></div>
+      <div className="mt-5">
+        <h1 className="text-white font-bold text-2xl hover:text-[1.6em] cursor-pointer" >My Pokemons</h1>
+        <button 
+          onClick={fetchCapturedPokemons}
+          className="text-white bg-amber-500 m-10 p-4 rounded-md font-bold text-2xl hover:bg-amber-600"
+        >
+          Ver meu time
+        </button>
+        <div>
+          {capturedPokemons.length > 0 ? (
+            <ul>
+              {capturedPokemons.map((pokemon: any) => (
+                <li key={pokemon.id}>{pokemon.name}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>Você não tem Pokémons capturados ainda.</p>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      {message && <p className="text-white">{message}</p>}
+      </div>
     </div>
   );
 }
