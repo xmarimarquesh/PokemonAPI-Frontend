eaa7e87d221e8276eaae0c888efee98b1048e8a6
xmarimarquesh <marimarquesh39@gmail.com>
2025-03-13 16:57:22 -0300

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,78 @@
-"use client";
 import Image from "next/image";
-import { useState } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
-
-  const [pokemonId, setPokemonId] = useState<number>(1);
-  const [message, setMessage] = useState<string>('');
+  const [pokemonId, setPokemonId] = useState<number>(0);
+  const [pokemonName, setPokemonName] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
   const [capturedPokemons, setCapturedPokemons] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchRandomPokemon();
+  }, []);
+
+  const fetchRandomPokemon = async () => {
+    try {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1");
+      const totalPokemons = response.data.count; 
+      const randomId = Math.floor(Math.random() * totalPokemons) + 1;
+      
+      console.log("Total: ",totalPokemons)
+
+      const pokemonResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId.toString()}`);
+      setPokemonId(randomId); 
+      setPokemonName(pokemonResponse.data.name); 
+    } catch (error) {
+      console.error("Erro ao buscar Pokémon aleatório:", error);
+    }
+  };
 
   const capturePokemon = async () => {
     try {
-      const response = await axios.post('/localhost:/8080/capture', { id: pokemonId }); 
+      const response = await axios.post("http://localhost:8080/api/capture", { id: pokemonId });
       setMessage(`Pokémon ${response.data.pokemon.name} capturado com sucesso!`);
-    } catch (error:any) {
-      setMessage(error.response?.data?.message || 'Erro na captura!');
+    } catch (error: any) {
+      setMessage(error.response?.data?.message || "Erro na captura!");
     }
   };
 
   const fetchCapturedPokemons = async () => {
     try {
-      const response = await axios.get('/team');
+      const response = await axios.get("http://localhost:8080/api/team");
       setCapturedPokemons(response.data);
     } catch (error) {
-      console.error('Erro ao buscar Pokémons capturados');
+      console.error("Erro ao buscar Pokémons capturados");
     }
   };
 
   return (
     <div className="bg-root flex flex-col items-center">
-      <Image src={require('@/assets/capa.jpg')} alt="" className="h-100 object-cover"></Image>
+      <Image src={require("@/assets/capa.jpg")} alt="" className="h-100 object-cover" />
       <div className="h-30">
-        <h1 className="text-amber-500 font-bold text-[2.4em] mt-10 hover:text-[2.6em] cursor-pointer transform-stroke">Pokemon - API</h1>
+        <h1 className="text-amber-500 font-bold text-[2.4em] mt-10 hover:text-[2.6em] cursor-pointer transform-stroke">
+          Pokemon - API
+        </h1>
       </div>
+
       <div className="h-30">
-        <button 
+        <h2 className="text-white text-xl">Pokémon Aleatório: {pokemonName}</h2>
+      </div>
+
+      <div className="h-30">
+        <button
           onClick={capturePokemon}
-          className="text-white bg-amber-500 m-10 p-4 rounded-md font-bold text-2xl hover:bg-amber-600">
+          className="text-white bg-amber-500 m-10 p-4 rounded-md font-bold text-2xl hover:bg-amber-600"
+        >
           Tentar capturar
         </button>
       </div>
-      <div className="w-[80%] bg-white h-[1px]"></div>
+      
+      <div className="w-[80%] bg-white h-[1px]" />
+
       <div className="mt-5">
-        <h1 className="text-white font-bold text-2xl hover:text-[1.6em] cursor-pointer" >My Pokemons</h1>
-        <button 
+        <h1 className="text-white font-bold text-2xl hover:text-[1.6em] cursor-pointer">My Pokemons</h1>
+        <button
           onClick={fetchCapturedPokemons}
           className="text-white bg-amber-500 m-10 p-4 rounded-md font-bold text-2xl hover:bg-amber-600"
         >
@@ -53,14 +82,15 @@ export default function Home() {
           {capturedPokemons.length > 0 ? (
             <ul>
               {capturedPokemons.map((pokemon: any) => (
-                <li key={pokemon.id}>{pokemon.name}</li>
+                <li className="text-white" key={pokemon.id}>{pokemon.name}</li>
               ))}
             </ul>
           ) : (
-            <p>Você não tem Pokémons capturados ainda.</p>
+            <p className="text-white">Você não tem Pokémons capturados ainda.</p>
           )}
         </div>
-      {message && <p className="text-white">{message}</p>}
+
+        {message && <p className="text-white">{message}</p>}
       </div>
     </div>
   );
